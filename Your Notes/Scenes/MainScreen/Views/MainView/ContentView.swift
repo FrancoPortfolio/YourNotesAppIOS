30430befb5a435ee3ea81cd7c438e0e95c1de01a
Franco Marquez <20191218@aloe.ulima.edu.pe>
2023-10-16 10:05:10 -0500

    Initial Commit

---
+++ Your Notes/Scenes/MainScreen/Views/MainView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Your Notes
+//
+//  Created by Franco Marquez on 16/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Your Notes/Your_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_NotesApp.swift
+//  Your Notes
+//
+//  Created by Franco Marquez on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Your_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
